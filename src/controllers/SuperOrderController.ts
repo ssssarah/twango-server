941898e20e37f1c43a5c329b7a9484d7f17a59f8
ssssarah <mouffoksarah1@gmail.com>
2019-04-16 20:41:54 +0200

    same line both drivers?

--- src/controllers/SuperOrderController.ts
+++ src/controllers/SuperOrderController.ts
@@ -179,9 +179,8 @@ class SuperOrderController {
                                                                 .take(RESULT_PER_PAGE)
                                                                 .skip((page-1) * RESULT_PER_PAGE);
 
-            queryBuilder.where('"isDeleted" = false'); //POSTGRES
-//            queryBuilder.where("isDeleted = 0"); //MYSQL
-
+            queryBuilder.where("super_order.isDeleted = :isDeleted", {isDeleted: false});
+            
             // {type: "deadline", order:"ASC/DESC"}
             if(isDefined(sortType) && isDefined(sortOrder)){
                 if(sortOrder != "ASC" && sortOrder != "DESC"){
