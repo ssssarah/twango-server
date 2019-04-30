d5330a8a1e8dcfd6be4d323707bb7fe5184c6e14
ssssarah <mouffoksarah1@gmail.com>
2019-04-30 13:23:46 +0200

    update validation unique

--- src/UniqueField.ts
+++ src/UniqueField.ts
@@ -9,7 +9,12 @@ export class UniqueField implements ValidatorConstraintInterface {
     validate(value: any, args: ValidationArguments) {
         let obj = {};
         obj[args.constraints[1]] = value;
-        return getRepository(args.constraints[0]).findOne(obj).then(entity => !entity);
+        return getRepository(args.constraints[0]).findOne(obj).then(entity => {
+            if(entity)
+                return entity[args.constraints[1]] == value;
+            // if entity found, check old value is the same as new, in which case its not duplication
+            return !entity;
+        });
     }
 
     defaultMessage(args: ValidationArguments) {
