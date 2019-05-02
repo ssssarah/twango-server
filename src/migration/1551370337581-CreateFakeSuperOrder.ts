472a0642a34a1b9952ebc7736fbc8522372cc2fe
ssssarah <mouffoksarah1@gmail.com>
2019-05-02 23:15:48 +0200

    test data

--- src/migration/1551370337581-CreateFakeSuperOrder.ts
+++ src/migration/1551370337581-CreateFakeSuperOrder.ts
@@ -12,27 +12,35 @@ export class CreateFakeSuperOrder1551370337581 implements MigrationInterface {
 
         for(let key in allUsers){
             let user = allUsers[key];
-            let superOrder : SuperOrder = new SuperOrder();
-            superOrder.user = user;
-            superOrder.storeURL = faker.internet.url();
-            superOrder.storeLocation = faker.address.city();
-            superOrder.storeName = faker.internet.domainName();
-            superOrder.imageUrl = faker.image.imageUrl(300,300);
+            let nb = user.username === "test" ? 10 : 1;
 
-            let randomDispatch = Math.random();
-            superOrder.availableDispatch = randomDispatch > 0.7 ? Dispatch.PICKUP :
-                (randomDispatch > 0.3 ? Dispatch.DELIVERY : Dispatch.BOTH);
+            for(let i = 0; i < nb; ++i){
 
-            superOrder.deadline = faker.date.future();
-            superOrder.arrivalLocation = faker.address.city();
-            superOrder.tags = faker.random.words(5).split(" ");
-            await superOrderRepository.save(superOrder);
+                let superOrder : SuperOrder = new SuperOrder();
+                superOrder.user = user;
+                superOrder.storeURL = faker.internet.url();
+                superOrder.storeLocation = faker.address.city();
+                superOrder.storeName = faker.internet.domainName();
+                superOrder.imageUrl = faker.image.imageUrl(300,300);
+
+                let randomDispatch = Math.random();
+                superOrder.availableDispatch = randomDispatch > 0.7 ? Dispatch.PICKUP :
+                    (randomDispatch > 0.3 ? Dispatch.DELIVERY : Dispatch.BOTH);
+
+                superOrder.deadline = faker.date.future();
+                superOrder.arrivalLocation = faker.address.city();
+                superOrder.tags = faker.random.words(5).split(" ");
+                await superOrderRepository.save(superOrder);
+            }
+
         }
     }
 
     public async down(queryRunner: QueryRunner): Promise<any> {
         getRepository(SuperOrder).delete({});
     }
+
+
 
 }
 
